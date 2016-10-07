d5b60d3e52fd45f5ff23e069bcf3ac9f4ab26eeb
Jay Koontz <jkoontz2010@gmail.com>
2016-10-06 19:10:22 -0700

    add parent component w/ bindings

--- src/app/containers/App.js
+++ src/app/containers/App.js
@@ -1,6 +1,31 @@
 class App {
-  constructor() {
-    this.restaurants = [initialRestaurants];
+  constructor($uibModal, $firebaseAuth) {
+    this.restaurants = [];
+    this.$uibModal = $uibModal;
+    this.authHelper = $firebaseAuth();
+    this.user = null;
+
+    this.authHelper.$onAuthStateChanged(user => {
+      this.user = user;
+    });
+  }
+
+  openLoginModal() {
+    const modalInstance = this.$uibModal.open({
+      component: 'loginForm',
+      animation: true
+    });
+  }
+
+  openSignupModal() {
+    const modalInstance = this.$uibModal.open({
+      component: 'signupForm',
+      animation: true
+    });
+  }
+
+  logoutUser() {
+    this.authHelper.$signOut();
   }
 }
 
